64dc01087997d6309b09112c0488069a5edfd9ba
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-08-15 14:15:11 +0200

    updates version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ PACKAGE_NAME = "unify_integration"
 
 setup(
     name=PACKAGE_NAME,
-    version="0.0.13",
+    version="0.0.14",
     description="The Unify tool package gives access to a single sign on client with multiple LLM endpoints and their metadata",  # noqa: E501
     packages=find_packages(),
     entry_points={
