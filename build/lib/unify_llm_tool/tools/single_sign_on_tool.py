0c5b49b3984d2479764977a5a219670ddd7e741e
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-08-15 12:48:22 +0200

    wheel update with custom_connection branch

--- build/lib/unify_llm_tool/tools/single_sign_on_tool.py
+++ build/lib/unify_llm_tool/tools/single_sign_on_tool.py
@@ -1,11 +1,14 @@
-from typing import Dict, Optional
+from typing import Dict, Optional, Any, List
 
 import unify.clients
 from unify import Unify
+import unify
 
+from promptflow.contracts.types import Secret
 from promptflow._constants import ConnectionType
 from promptflow.connections import CustomConnection
 from promptflow.core import tool
+
 
 
 class UnifyClient(Unify):
@@ -45,6 +48,8 @@ class UnifyConnection(CustomConnection):
     :param kwargs: Additional keyword arguments
     :type kwargs: dict
     """
+    api_key: Secret
+    api_base: str = "https://api.unify.ai/v0"
 
     TYPE = ConnectionType.CUSTOM.value
     _Connection_kwargs: dict = {
@@ -61,9 +66,10 @@ class UnifyConnection(CustomConnection):
     ):
 
         kwargs = {**kwargs, **self._Connection_kwargs}
+        self.convert_to_strong_type()
         super().__init__(secrets=secrets, configs=configs, **kwargs)
 
-    def connect(self) -> Unify:
+    def convert_to_strong_type(self) -> Unify:
         """
         Creates an instance of Unify client.
 
@@ -74,16 +80,74 @@ class UnifyConnection(CustomConnection):
         return self._convert_to_custom_strong_type(module=module, to_class=name)
 
 
+def list_endpoints(model: Optional[str], provider: Optional[str], api_key: Optional[str]) -> List[str]:
+    """
+    Lists endpoints available through Unify.
+
+    :param model: If provided, filters the list of endpoints for the ones using the provided model.
+    :type param: str
+    :param provider: If provided, filters the list of endpoints for the ones using the provider.
+    :type provider: str
+    :param api_key: Unify API key
+    :type api_key: str
+    """
+    try:
+        return unify.list_endpoints(model=model, provider=provider, api_key=api_key)
+    except ValueError:
+        return unify.list_endpoints(model=model)
+
+
+def list_models(provider: Optional[str], api_key: Optional[str]) -> List[str]:
+    """
+    Lists models available through Unify.
+
+    :param provider: Provider for which to generate the list of the available models.
+    :type provider: str
+    :param api_key:
+    :type api_key: str
+    """
+    try:
+        return unify.list_models(provider=provider, api_key=api_key)
+    except ValueError:
+        return unify.list_models()
+
+
+def list_providers(model: Optional[str], api_key: Optional[str]) -> List[str]:
+    """
+    Lists providers available through Unify.
+
+    :param model: Model for which to generate the list of the available providers.
+    :type provider: str
+    :param api_key:
+    :type api_key: str
+    """
+    try:
+        return unify.list_providers(model=model, api_key=api_key)
+    except ValueError:
+        return unify.list_providers(model=model)
+
+
 @tool
-def single_sign_on(configs: Optional[Dict[str, str]], secrets: Dict[str, str]) -> UnifyConnection:
+def single_sign_on(
+    connection: UnifyConnection,
+    endpoint: Optional[str],
+    model: Optional[str],
+    provider: Optional[str],
+    router: Optional[str],
+) -> Unify:
     """Unify connection tool.
 
-    :param configs: The configs kv pairs.
-    :type configs: Dict[str, str]
-    :param secrets: The secrets kv pairs.
-    :type secrets: Dict[str, str]
+    :param endpoint: The endpoint to use.
+    :type endpoint: str
+    :param model: The model to use.
+    :type model: str
+    :param provider: The provider from the list of providers available for the model.
+    :type provider: str
     """
-    connection = UnifyConnection(secrets=secrets, configs=configs)
-    connection_instance = connection.connect()
-
-    return connection_instance
+    if new_endpoint := endpoint or router:
+        connection.set_endpoint(new_endpoint)
+        return connection
+    if model and provider:
+        connection.set_model(model)
+        connection.set_provider(provider)
+        return connection
