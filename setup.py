0d2df7c295675891554fbd89e5620405814deed4
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-09-23 14:01:30 +0200

    updates pyproject.toml

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ PACKAGE_NAME = "unify_integration"
 
 setup(
     name=PACKAGE_NAME,
-    version="0.1.1",
+    version="0.1.11",
     description="The Unify tool package gives access to a single sign on client with multiple LLM endpoints and their metadata",  # noqa: E501
     packages=find_packages(),
     extras_require={"dev": ["requests_mock", "python_dotenv"], "build": ["promptflow", "unifyai"]},
